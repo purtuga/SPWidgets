31d7b6f4ae5974202cc65370fd110b1840611b87
Paul T <paultavares1@gmail.com>
2013-04-05 14:09:24 -0400

    New method - SPGetDateString()

--- src/jquery.SPWidgets.js
+++ src/jquery.SPWidgets.js
@@ -342,6 +342,56 @@
         
     };// $.SPWidgets.getCamlLogical()
     
+    /**
+     * Returns a date string in the format expected by Sharepoint
+     * Date/time fields. Usefaul in doing filtering queries.
+     * 
+     * Credit:  Matt (twitter @iOnline247)
+     *          {@see http://spservices.codeplex.com/discussions/349356}
+     * 
+     * @param {Date} [dateObj=Date()]
+     * @param {String} [formatType='local']
+     *              Possible formats: local, utc
+     * 
+     * @return {String} a date string.
+     * 
+     */
+    $.SPWidgets.SPGetDateString = function( dateObj, formatType ) {
+        
+        formatType  = String(formatType || "local").toLowerCase();
+        dateObj     = dateObj || new Date();
+    
+        function pad( n ) {
+            
+            return n < 10 ? '0' + n : n;
+            
+        }
+        
+        var ret = '';
+        
+        if (formatType === 'utc') {
+            
+            ret = dateObj.getUTCFullYear() + '-' +
+                    pad( dateObj.getUTCMonth() + 1 ) + '-' +
+                    pad( dateObj.getUTCDate() ) + 'T' +
+                    pad( dateObj.getUTCHours() ) + ':' +
+                    pad( dateObj.getUTCMinutes() )+ ':' +
+                    pad( dateObj.getUTCSeconds() )+ 'Z';
+
+        } else {
+            
+            ret = dateObj.getFullYear() + '-' +
+                    pad( dateObj.getMonth() + 1 ) + '-' +
+                    pad( dateObj.getDate() ) + 'T' +
+                    pad( dateObj.getHours() ) + ':' +
+                    pad( dateObj.getMinutes() )+ ':' +
+                    pad( dateObj.getSeconds() );
+            
+        }
+        
+        return ret;
+                
+    }; //end: $.SPWidgets.SPGetDateString()
     
 //_SPWIDGETS_PLUGINS_
 
