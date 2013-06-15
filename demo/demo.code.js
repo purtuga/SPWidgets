c0a0d0c7826a2d0a2a45b673363a46a94cb173c3
Paul T <paultavares1@gmail.com>
2013-06-15 18:09:01 -0400

    Demo updated to show new feature of pickSPUser()

--- demo/demo.code.js
+++ demo/demo.code.js
@@ -216,21 +216,77 @@
         }//end: completefunc()
     });
 
+    //-----------------------------------
     // pickSPUser plugin
-    $("input[name='spuserdemo']").pickSPUser({
-        onPickUser: function(u){
-            $("#sp_control_pick_user_detail")
-                .empty()
-                .append(
-                    '<div>The following User was selected:</div>' +
-                    '<div>User Name: ' + u.displayName + '</div>' +
-                    '<div>Account Name: ' + u.accountName + '</div>' +
-                    '<div>Account ID: ' + u.accountId + '</div>' +
-                    '<div>Account Type: ' + u.accountType + '</div>'
-                );
+    //-----------------------------------
+    $("#SPControlPickUserDemo").each(function(){
+        
+        var $cntr   = $(this),
+            $output = $cntr.find("#SPControlPickUserEventOut");
+        
+        /**
+         * Logs output to the people picker output area
+         */
+        function logOutput(data) {
+            
+            $output.append($("<div/>").html(data));
+            
         }
-    });
-    
+        
+        // SEtup listners
+        $cntr
+            .on("spwidget:peoplePickerCreate", function(ev, $input){
+                
+                logOutput("spwidget:peoplePickerCreate EVENT TRIGGERED!");
+                
+            })
+            .on("spwidget:peoplePickerAdd", function(ev, $input, personObj){
+                
+                logOutput(
+                    "spwidget:peoplePickerAdd TRIGGERED! (Person: " + 
+                    personObj.displayName + ")");
+                    
+            })
+            .on("spwidget:peoplePickerRemove", function(ev, $input, personObj){
+                
+                logOutput(
+                    "spwidget:peoplePickerRemove TRIGGERED! (Person: " + 
+                    personObj.displayName + ")");
+                    
+            });
+        
+        // Attach widget to input.
+        $("input[name='spuserdemo']").pickSPUser({
+            onPickUser: function(u){
+                
+                logOutput("onPickUser CALLED!(Person: " + u.displayName + ")");
+                
+                $("#sp_control_pick_user_detail")
+                    .empty()
+                    .append(
+                        '<div>The following User was selected:</div>' +
+                        '<div>User Name: ' + u.displayName + '</div>' +
+                        '<div>Account Name: ' + u.accountName + '</div>' +
+                        '<div>Account ID: ' + u.accountId + '</div>' +
+                        '<div>Account Type: ' + u.accountType + '</div>'
+                    );
+            },
+            onCreate: function($input) {
+                
+                logOutput("onCreate CALLED!");
+                
+            },
+            onRemoveUser: function($input, $ui, person){
+                
+                logOutput("onRemoveUser CALLED! (Person: " + person.displayName + ")");
+                
+            }
+        });
+        
+        
+        
+        
+    }); //end: each() = People picker container
     
     // Kan-Ban Board
     $("#SPControlBoardDemo div.spwidget-board-demo-cntr")
