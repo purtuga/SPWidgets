046e1e57f49577dce133d5336e08244ef318c0fa
Paul T <paultavares1@gmail.com>
2013-09-02 17:19:33 -0400

    New Utility - $.SPWidgets.parseDateString

--- src/jquery.SPWidgets.js
+++ src/jquery.SPWidgets.js
@@ -379,7 +379,7 @@
         
         /**
          * Returns a date string in the format expected by Sharepoint
-         * Date/time fields. Usefaul in doing filtering queries.
+         * Date/time fields. Usefull in doing filtering queries.
          * 
          * Credit:  Matt (twitter @iOnline247)
          *          {@see http://spservices.codeplex.com/discussions/349356}
@@ -427,6 +427,131 @@
             return ret;
                     
         }; //end: $.SPWidgets.SPGetDateString()
+        
+        /**
+         * Parses a date string in ISO 8601 format into a Date object.
+         * Date format supported on input:
+         *  2013-09-01T01:00:00
+         *  2013-09-01T01:00:00Z
+         *  2013-09-01T01:00:00Z+05:00
+         * 
+         * @param {String} dateString
+         *      The date string to be parsed.
+         * 
+         * @return {Date|Null}
+         *      If unable to parse string, a Null value will be returned.
+         * 
+         * @see {https://github.com/csnover/js-iso8601}
+         *      Method was developed using some of the code from js-iso8601
+         *      project on github by csnover.
+         * 
+         */
+        $.SPWidgets.parseDateString = function(dateString) {
+            
+            var dtObj       = null,
+                re, dtPieces, i, j, numericKeys, minOffset;
+            
+            if (!dateString) {
+                
+                return dtObj;
+                
+            }
+            
+            // let's see if Date.parse() can do it?
+            dtObj = Date.parse(dateString);
+            
+            if (dtObj) {
+                
+                return new Date(dtObj);
+                
+            }
+            
+            // Once we parse the date string, these locations
+            // in the array must be Numbers.
+            numericKeys = [ 1, 4, 5, 6, 7, 10, 11 ];
+            
+            // Define regEx
+            re = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/;
+            
+            // dtPieces:
+            //    [0]   
+            //    [1]   YYYY
+            //    [2]   MM
+            //    [3]   DD
+            //    [4]   HH
+            //    [5]   mm
+            //    [6]   ss
+            //    [7]   msec
+            //    [8]   Z
+            //    [9]   +|-
+            //    [10]  Z HH
+            //    [11]  Z mm
+            dtPieces    = dateString.match(re);
+            
+            
+            if( !dtPieces ){
+                
+                return dtObj;
+                
+            }
+            
+            for(i=0,j=numericKeys.length; i<j; i++){
+                
+                dtPieces[numericKeys[i]] = ~~dtPieces[numericKeys[i]];
+                
+            }
+            
+            // Month is "zero" based
+            --dtPieces[2];
+            
+            // Date specifed UTC Format?
+            if (dtPieces[8] === 'Z') {
+                
+                // do we need to calculate offset to minutes?
+                if (dtPieces[9] !== undefined) {
+                    
+                    minOffset = dtPieces[10] * 60 + dtPieces[11];
+    
+                    if (dtPieces[9] === '+') {
+                        
+                        minOffset = (- minOffset);
+                        
+                    }
+                    
+                    dtPieces[5] += minOffset;
+                    
+                }
+                
+                dtObj = new Date(
+                        Date.UTC(
+                            dtPieces[1],
+                            dtPieces[2],
+                            dtPieces[3],
+                            dtPieces[4],
+                            dtPieces[5],
+                            dtPieces[6],
+                            dtPieces[7]
+                        )
+                    );
+                    
+            // Else: Date was did not seem to be UTC. Do local.
+            } else {
+                
+                dtObj = new Date(
+                        dtPieces[1],
+                        dtPieces[2],
+                        dtPieces[3],
+                        dtPieces[4],
+                        dtPieces[5],
+                        dtPieces[6],
+                        dtPieces[7]
+                    );
+                
+            }
+            
+            return dtObj;
+            
+        }; //end: $.SPWidgets.parseDateString()
         
         /**
          * Make a set of element the same height by taking the height of
