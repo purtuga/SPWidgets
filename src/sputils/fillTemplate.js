be7594cb976637b1a040b46e7200911681e0bfdb
Paul T <paultavares1@gmail.com>
2016-03-26 09:48:40 -0400

    fillTemplate refactored to remove dependency on jQuery

--- src/sputils/fillTemplate.js
+++ src/sputils/fillTemplate.js
@@ -1,89 +1,78 @@
-define(['jquery'], function($){
+define(function(){
 
     /**
-     * An extreemly lightweight template engine for replacing
+     * An extremely lightweight template engine for replacing
      * tokens in the form of {{name}} with values from an object
      * or a list (array) of objects
      *
-     * @param {Object} tmplt
-     * @param {Object} data
+     * @function fillTemplate
+     * 
+     * @param {String|HTMLElement} template
+     * @param {Object|Array<Object>} data
+     *  The Object containing the data that will be applied to the
+     *  template. An array of objects can also be defined
      *
-     * @return {String} templated filled out
-     *
+     * @return {String}
      */
-    var fillTemplate = function fillTemplate(tmplt, data) {
+    return function fillTemplate(template, data) {
 
-        var opt = {},i,j,x,y,item, tokenVal;
+        var opt = {},
+            i,j,x,y,item, tokenVal, tmp;
 
         // If user used an object to define input param, then parse that now
-        if (typeof tmplt === "object" && arguments.length === 1) {
-
-            data    = tmplt.data;
-            tmplt   = tmplt.tmplt;
-
+        if (typeof template === "object" && arguments.length === 1) {
+            data        = template.data;
+            template    = template.template;
         }
 
-        opt.response    = "";
-        opt.template    = (
-            typeof tmplt !== "string" ?
-                String($("<div/>").append(tmplt).html()) :
-                tmplt
-        );
-        opt.tokens      = opt.template.match(/(\{\{.*?\}\})/g);
+        opt.response = "";
 
-        if (!$.isArray(data)) {
+        if (typeof template !== "string") {
+            tmp = document.createElement("div");
+            tmp.appendChild(template);
+            template = tmp.innerHTML;
 
+        } else {
+            opt.template = template;
+        }
+
+        opt.tokens = opt.template.match(/(\{\{.*?\}\})/g);
+
+        if (!Array.isArray(data)) {
             if (!data) {
-
                 data = [{}];
 
             } else {
-
                 data = [ data ];
-
             }
-
         }
 
         // If we have tokens in the template, then replace them
         if (opt.tokens !== null) {
-
             // If data tokens were passed in on input, then use them
             // in looking for that token in the template and replacing
             // it with the value defined.
             for(x=0,y=data.length; x<y; x++){
-
                 item = opt.template;
 
                 for(i=0,j=opt.tokens.length; i<j; i++){
-
                     opt.tokens[i]   = opt.tokens[i].replace(/[\{\}]/g, "");
                     tokenVal        = data[x][ opt.tokens[i] ] || '';
 
-                    if ($.isFunction(tokenVal)) {
-
+                    if (typeof tokenVal === "function") {
                         tokenVal = tokenVal();
-
                     }
 
                     item = item.replace("{{" + opt.tokens[i] + "}}", tokenVal);
-
                 }
 
                 opt.response += item;
-
             }
 
         } else {
-
             opt.response = opt.template;
-
         }
 
         return opt.response;
-
-    }; //end: fillTemplate()
-
-    return fillTemplate;
-
+    };
 });
