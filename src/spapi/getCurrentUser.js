5c852721eeed50c9e51caac46ecad8e696bef126
Paul T <paultavares1@gmail.com>
2017-01-14 15:55:04 -0500

    Fixes to getCurrentUser

--- src/spapi/getCurrentUser.js
+++ src/spapi/getCurrentUser.js
@@ -1,239 +1,247 @@
-import apiFetch from "../sputils/apiFetch";
-import cache from "../sputils/cache";
-import getSiteWebUrl from "./getSiteWebUrl";
-import searchPrincipals from "./searchPrincipals";
-import fetchPolyfill from "vendor/jsutils/es7-fetch";
-import objectExtend from "vendor/jsutils/objectExtend";
-import Promise from "vendor/jsutils/es6-promise";
-import parseHTML from "vendor/jsutils/parseHTML";
-import domFind from "vendor/domutils/domFind";
-    /* globals _spPageContextInfo  */
+import apiFetch         from "../sputils/apiFetch"
+import cache            from "../sputils/cache"
+import getSiteWebUrl    from "./getSiteWebUrl"
+import searchPrincipals from "./searchPrincipals"
+import fetchPolyfill    from "vendor/jsutils/es7-fetch"
+import objectExtend     from "vendor/jsutils/objectExtend"
+import Promise          from "vendor/jsutils/es6-promise"
+import parseHTML        from "vendor/jsutils/parseHTML"
+import domFind          from "vendor/domutils/domFind"
 
+//-----------------------------------------------------------------------
+/* globals _spPageContextInfo  */
+
+let fetch           = fetchPolyfill.fetch;
+const PROMISE_CATCH = "catch";
+const consoleLog    = console.log.bind(console);  // jshint ignore:line
+
+/**
+ * Returns a `UserProfileModel` that represents the currently logged in user.
+ *
+ * @param {Object} [options]
+ * @param {String} [options.webURL=Current_Site]
+ *
+ * @return {Promise<UserProfileModel, Error>}
+ *  Promise is resolved with an UserProfileModel.
+ *
+ *  // FIXME: backward compatibility:
+ *      userLoginName
+ *      userId
+ *
+ */
+const getCurrentUser = function(options){
     var
-    fetch           = fetchPolyfill.fetch,
-    PROMISE_CATCH   = "catch",
-    consoleLog      = console.log.bind(console),  // jshint ignore:line
+    cacheId     = "getCurrentUserData",
+    opt         = objectExtend({}, getCurrentUser.defaults, options),
+    reqPromise  =  Promise.resolve();
 
-    /**
-     * Returns a `UserProfileModel` that represents the currently logged in user.
-     *
-     * @param {Object} [options]
-     * @param {String} [options.webURL=Current_Site]
-     *
-     * @return {Promise<UserProfileModel, Error>}
-     *  Promise is resolved with an UserProfileModel.
-     *
-     *  // FIXME: backward compatibility:
-     *      userLoginName
-     *      userId
-     *
-     */
-    getCurrentUser = function(options){
-        var
-        cacheId     = "getCurrentUserData",
-        opt         = objectExtend({}, getCurrentUser.defaults, options),
-        reqPromise  =  Promise.resolve()
-            .then(function(){
-                if (cache.isCached(cacheId)) {
-                    return cache.get(cacheId);
+    reqPromise.then(function(){
+        if (cache.isCached(cacheId)) {
+            return cache.get(cacheId);
+        }
+
+        cache(cacheId, reqPromise);
+
+        return searchUserPrincipals(opt);
+
+    })[PROMISE_CATCH](function(e){
+        consoleLog(e);
+        return scrapeUserDisplayPage(opt);
+
+    // Unable to get current user
+    })[PROMISE_CATCH](function(e){
+        consoleLog(e); // jshint ignore:line
+        return Promise.reject(new Error("Unable to get current user info."));
+    });
+
+    return reqPromise;
+};
+
+/**
+ * Uses the searchPrincipals to try to identify the current user using
+ * the userLoginName information from `_spPageContextInfo.userLoginName`
+ *
+ * @private
+ *
+ * @param {Object} opt
+ *
+ * @returns {Promise<UserProfileModel, Error>}
+ */
+function searchUserPrincipals(opt) {
+
+    // Possible second approach?: use 'GetUserInfo' service which accepts a loginName?
+    //      API: https://msdn.microsoft.com/en-us/library/ms774637.aspx
+
+    return new Promise(function(resolve, reject){
+        if (
+            typeof _spPageContextInfo               === "undefined" ||
+            typeof _spPageContextInfo.userLoginName === "undefined" ||
+            typeof _spPageContextInfo.userId        === "undefined"
+        ) {
+            reject(new Error('Unable to searchPrincipals for user. Needed info not in _spPageContextInfo'));
+            return;
+        }
+
+        var userId          = String(_spPageContextInfo.userId),
+            userLoginName   = _spPageContextInfo.userLoginName;
+
+        searchPrincipals({
+            searchText: userLoginName,
+            webUrl:     opt.webURL
+        })
+        .then(function(results){
+            var userProfile;
+
+            results.some(function(user){
+                if (String(user.ID) === userId) {
+                    userProfile = user;
+                    return true;
                 }
-
-                cache(cacheId, reqPromise);
-
-                return searchUserPrincipals(opt);
-
-            })[PROMISE_CATCH](function(e){
-                consoleLog(e);
-                return scrapeUserDisplayPage(opt);
-
-            // Unable to get current user
-            })[PROMISE_CATCH](function(e){
-                consoleLog(e); // jshint ignore:line
-                return Promise.reject(new Error("Unable to get current user info."));
             });
 
-        return reqPromise;
-    };
-
-    /**
-     * Uses the searchPrincipals to try to identify the current user using
-     * the userLoginName information from `_spPageContextInfo.userLoginName`
-     *
-     * @private
-     *
-     * @param {Object} opt
-     *
-     * @returns {Promise<UserProfileModel, Error>}
-     */
-    function searchUserPrincipals(opt) {
-
-        // Possible second approach?: use 'GetUserInfo' service which accepts a loginName?
-        //      API: https://msdn.microsoft.com/en-us/library/ms774637.aspx
-
-        return new Promise(function(resolve, reject){
-            if (
-                typeof _spPageContextInfo               === "undefined" ||
-                typeof _spPageContextInfo.userLoginName === "undefined" ||
-                typeof _spPageContextInfo.userId        === "undefined"
-            ) {
-                reject(new Error('Unable to searchPrincipals for user. Needed info not in _spPageContextInfo'));
+            if (!userProfile) {
+                reject(new Error("User not found via searchPrincipals"));
                 return;
             }
 
-            var userId          = String(_spPageContextInfo.userId),
-                userLoginName   = _spPageContextInfo.userLoginName;
+            resolve(userProfile);
 
-            searchPrincipals({
-                searchText: userLoginName,
-                webUrl:     opt.webURL
+        })[PROMISE_CATCH](function(e){
+            reject(e);
+        });
+    });
+}
+
+/**
+ * Same approach as SPServices - screen scrape the user Display page.
+ *
+ * @private
+ *
+ * @return {Promise}
+ */
+function scrapeUserDisplayPage(opt) {
+    // NOTE:
+    // Although the API differs, this method implementation borrows heavily from
+    // SPServices.SPGetCurrentUser (http://spservices.codeplex.com/)
+
+    return getSiteWebUrl(opt.webURL).then(function(webURL){
+
+        return fetch(webURL + "/_layouts/userdisp.aspx?Force=True&" + (new Date()).getTime())
+            .then(function(response){
+                return response.text().then(function(responseString){
+                    return parseHTML(responseString);
+                });
             })
-            .then(function(results){
-                var userProfile;
+            .then(function(contentDocFrag){
+                var userInfo = {};
 
-                results.some(function(user){
-                    if (String(user.ID) === userId) {
-                        userProfile = user;
-                        return true;
+                domFind(contentDocFrag, "table.ms-formtable td.ms-formbody").forEach(function($td){
+                    var
+                    tdInnerHtml     = $td.innerHTML,
+                    reInternalName  = /FieldInternalName="(.+?)"/i,
+                    reFieldType     = /FieldType="(.+?)"/i,
+                    internalName    = reInternalName.exec(tdInnerHtml),
+                    fieldType       = reFieldType.exec(tdInnerHtml),
+                    key, value, $child;
+
+                    // If we found an internal name, then use that as the key
+                    if (internalName) {
+                        key = internalName[1];
+
+                    // ELSE, use the visible label on the page
+                    } else {
+                        key = $td.previousSibling.textContent.trim();
                     }
+
+                    fieldType = (fieldType ? fieldType[1] : "");
+
+                    // Get the value for this field
+                    switch (fieldType) {
+                        case "SPFieldNote":
+                            $child = $td.querySelector("div");
+                            if ($child) {
+                                value = $child.innerHTML;
+                            }
+                            break;
+
+                        case "SPFieldURL":
+                            $child = $td.querySelector("img");
+                            if ($child) {
+                                value = $child.getAttribute("src");
+                            }
+                            break;
+
+                        default:
+                            value = $td.textContent;
+                            break;
+                    }
+
+                    userInfo[key] = (value || "").trim();
                 });
 
-                if (!userProfile) {
-                    reject(new Error("User not found via searchPrincipals"));
-                    return;
+
+                // If no ID, then get it from the Edit link
+                if (!userInfo.hasOwnProperty("ID")) {
+
+                    domFind(contentDocFrag, "table.ms-toolbar a[id*='EditItem']").some(function(ele){
+                        var idMatch = (/ID=(\d*)/i).exec(ele.href);
+                        if (idMatch) {
+                            userInfo.ID = idMatch[1];
+                            return true;
+                        }
+                    });
                 }
 
-                resolve(userProfile);
+                // If no 'AccountName', and we have "Name", then set it from that
+                // (since it seems that it is the account name)
+                if (!userInfo.AccountName && userInfo.Name) {
+                    userInfo.AccountName = userInfo.Name;
+                }
 
-            })[PROMISE_CATCH](function(e){
-                reject(e);
+                if (userInfo.ID) {
+                    // FIXME: need to use getUserProfile now to get good profile
+
+                    return userInfo;
+                }
+
+                return Promise.reject(new Error("Unable to get ID from scraping userdisp.aspx."));
+
+                // EXAMPLE OF DATA RETRIEVED FROM SCRAPE:
+                //{
+                //    "Name": "i:0#.f|membership|joe.doe@sharepoint.com",
+                //    "AccountName": "Joe Doe", // Set from "title"
+                //    "Title": "Joe Doe",
+                //    "EMail": "joedoe@sharepoint.com",
+                //    "MobilePhone": "",
+                //    "Notes": "<div dir=\"\"></div>&nbsp;",
+                //    "Picture": "https://my.sharepoint.com:443/User%20Photos/Profile%20Pictures/joeDoe.jpg?t=63523359043",
+                //    "Department": "",
+                //    "JobTitle": "",
+                //    "SipAddress": "",
+                //    "FirstName": "joe",
+                //    "LastName": "doe",
+                //    "WorkPhone": "",
+                //    "UserName": "joe.doe@sympraxisconsulting.com",
+                //    "WebSite": "",
+                //    "SPSResponsibility": "",
+                //    "Office": "",
+                //    "SPSPictureTimestamp": "63523359043",
+                //    "SPSPicturePlaceholderState": "",
+                //    "SPSPictureExchangeSyncState": "",
+                //    "Attachments": "",
+                //    "ID": "11"
+                //}
+
             });
-        });
-    }
-
-    /**
-     * Same approach as SPServices - screen scrape the user Display page.
-     *
-     * @private
-     *
-     * @return {Promise}
-     */
-    function scrapeUserDisplayPage(opt) {
-        // NOTE:
-        // Although the API differs, this method implementation borrows heavily from
-        // SPServices.SPGetCurrentUser (http://spservices.codeplex.com/)
-
-        return getSiteWebUrl(opt.webURL).then(function(webURL){
-
-            return fetch(webURL + "/_layouts/userdisp.aspx?Force=True&" + (new Date()).getTime())
-                .then(function(response){
-                    return response.text().then(function(responseString){
-                        return parseHTML(responseString);
-                    });
-                })
-                .then(function(contentDocFrag){
-                    var userInfo = {};
-
-                    domFind(contentDocFrag, "table.ms-formtable td.ms-formbody").forEach(function($td){
-                        var
-                        tdInnerHtml     = $td.innerHTML,
-                        reInternalName  = /FieldInternalName="(.+?)"/i,
-                        reFieldType     = /FieldType="(.+?)"/i,
-                        internalName    = reInternalName.exec(tdInnerHtml),
-                        fieldType       = reFieldType.exec(tdInnerHtml),
-                        key, value, $child;
-
-                        // If we found an internal name, then use that as the key
-                        if (internalName) {
-                            key = internalName[1];
-
-                        // ELSE, use the visible label on the page
-                        } else {
-                            key = $td.previousSibling.textContent.trim();
-                        }
-
-                        fieldType = (fieldType ? fieldType[1] : "");
-
-                        // Get the value for this field
-                        switch (fieldType) {
-                            case "SPFieldNote":
-                                $child = $td.querySelector("div");
-                                if ($child) {
-                                    value = $child.innerHTML;
-                                }
-                                break;
-
-                            case "SPFieldURL":
-                                $child = $td.querySelector("img");
-                                if ($child) {
-                                    value = $child.getAttribute("src");
-                                }
-                                break;
-
-                            default:
-                                value = $td.textContent;
-                                break;
-                        }
-
-                        userInfo[key] = (value || "").trim();
-                    });
+    });
+}
 
 
-                    // If no ID, then get it from the Edit link
-                    if (!userInfo.hasOwnProperty("ID")) {
+getCurrentUser.defaults = {
+    webURL: null
+};
 
-                        domFind(contentDocFrag, "table.ms-toolbar a[id*='EditItem']").some(function(ele){
-                            var idMatch = (/ID=(\d*)/i).exec(ele.href);
-                            if (idMatch) {
-                                userInfo.ID = idMatch[1];
-                                return true;
-                            }
-                        });
-                    }
-
-
-                    if (userInfo.ID) {
-                        // FIXME: need to use getUserProfile now to get good profile
-
-                        return userInfo;
-                    }
-
-                    return Promise.reject(new Error("Unable to get ID from scraping userdisp.aspx."));
-
-                    // EXAMPLE OF DATA RETRIEVED FROM SCRAPE:
-                    //{
-                    //    "Name": "i:0#.f|membership|joe.doe@sharepoint.com",
-                    //    "Title": "Joe Doe",
-                    //    "EMail": "joedoe@sharepoint.com",
-                    //    "MobilePhone": "",
-                    //    "Notes": "<div dir=\"\"></div>&nbsp;",
-                    //    "Picture": "https://my.sharepoint.com:443/User%20Photos/Profile%20Pictures/joeDoe.jpg?t=63523359043",
-                    //    "Department": "",
-                    //    "JobTitle": "",
-                    //    "SipAddress": "",
-                    //    "FirstName": "joe",
-                    //    "LastName": "doe",
-                    //    "WorkPhone": "",
-                    //    "UserName": "joe.doe@sympraxisconsulting.com",
-                    //    "WebSite": "",
-                    //    "SPSResponsibility": "",
-                    //    "Office": "",
-                    //    "SPSPictureTimestamp": "63523359043",
-                    //    "SPSPicturePlaceholderState": "",
-                    //    "SPSPictureExchangeSyncState": "",
-                    //    "Attachments": "",
-                    //    "ID": "11"
-                    //}
-
-                });
-        });
-    }
-
-
-    getCurrentUser.defaults = {
-        webURL: null
-    };
-
-    export default getCurrentUser;
+export default getCurrentUser;
 
     //------------------------------------------------
     // Other possible approaches
